27d9b0cdec5c7266d603620535b4004044e43e57
Samuel Fontes <samuguel@hotmail.com.br>
2024-05-21 10:19:23 -0500

    Generating some nice rectangles

--- Program.cs
+++ Program.cs
@@ -4,38 +4,46 @@ namespace OpenVegasPro;
 
 public static class Program
 {
-	public static void Main(string[] args)
-	{
-		// Set the width and height of the window     
-		int screenWidth = 800;
-		int screenHeight = 450;
+    public static void Main(string[] args)
+    {
+        // Set the width and height of the window     
+        int screenWidth = 800;
+        int screenHeight = 450;
 
-		// Set flags
-		Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
+        // Set flags
+        Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
 
-		// Initialize the window with the specified width, height, and title
-		Raylib.InitWindow(screenWidth, screenHeight, "OpenVegasPro");
+        // Initialize the window with the specified width, height, and title
+        Raylib.InitWindow(screenWidth, screenHeight, "OpenVegasPro");
 
-		// Set the FPS to 60
-		Raylib.SetTargetFPS(60);
+        // Set the FPS to 60
+        Raylib.SetTargetFPS(60);
 
-		// Loop until the window is closed
-		while (!Raylib.WindowShouldClose())
-		{
-			// Begin drawing to the window
-			Raylib.BeginDrawing();
 
-			// Clear the background to white
-			Raylib.ClearBackground(Raylib.RAYWHITE);
+        // Loop until the window is closed
+        while (!Raylib.WindowShouldClose())
+        {
+            screenWidth = Raylib.GetScreenWidth();
+            screenHeight = Raylib.GetScreenHeight();
 
-			// Draw the text "Hello World" in maroon color at position (190, 200)
-			Raylib.DrawText("Hello Raylib in CSharp!", 190, 200, 20, Raylib.MAROON);
 
-			// End drawing to the window
-			Raylib.EndDrawing();
-		}
+            // Begin drawing to the window
+            Raylib.BeginDrawing();
 
-		// Close the window
-		Raylib.CloseWindow();
-	}
+            // Clear the background to white
+            Raylib.ClearBackground(Raylib.RAYWHITE);
+            Raylib.DrawRectangle(0, 0, screenWidth, 32, Raylib.BLUE);
+            Raylib.DrawRectangle(0, 32, (int)(screenWidth * 0.4f), (int)(screenHeight * 0.5f), Raylib.RED);
+            Raylib.DrawRectangle((int)(screenWidth * 0.4f), 32, (int)(screenWidth * 0.6f), (int)(screenHeight * 0.5f), Raylib.GREEN);
+            Raylib.DrawRectangle(0, (int)(screenHeight * 0.5f) + 32, screenWidth, screenHeight - ((int)(screenHeight * 0.5f) + 32), Raylib.PURPLE);
+
+
+
+            // End drawing to the window
+            Raylib.EndDrawing();
+        }
+
+        // Close the window
+        Raylib.CloseWindow();
+    }
 }
