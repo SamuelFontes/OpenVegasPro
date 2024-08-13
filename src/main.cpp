246d952a099bf93fbf2d59fa633c4dd978640281
SamuelFontes <samuguel@hotmail.com.br>
2024-08-13 18:29:01 -0300

    Qt window working

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,18 @@
 #include "OpenVegasPro.h"
 #include <QApplication>
 #include <QDebug>
-
+#include <QtWidgets>
 
 
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
+	QWidget window;
+	window.resize(320, 240);
+	window.show();
+	window.setWindowTitle(
+		QApplication::translate("toplevel", "Top-level widget"));
 	return app.exec();
 }
