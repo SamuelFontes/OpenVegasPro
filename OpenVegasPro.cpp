cf25f049b4295449e29112dc82041555abc89f81
Samuel Fontes <samuguel@hotmail.com.br>
2024-08-13 09:52:45 -0500

    Added qt6 on cmake project

---
+++ OpenVegasPro.cpp
@@ -0,0 +1,12 @@
+﻿// OpenVegasPro.cpp : Defines the entry point for the application.
+//
+
+#include "OpenVegasPro.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
